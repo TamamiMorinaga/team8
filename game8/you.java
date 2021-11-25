c64023d3a288aa420c6bb4ed49e35420a1cb5767
TamamiMorinaga <17170@hakodate.kosen-ac.jp>
2021-11-25 11:38:20 +0900

    a

--- game8/you.java
+++ game8/you.java
@@ -51,6 +51,17 @@ public class you extends Actor
                 getImage().mirrorHorizontally();
             }
         }
+        /*if( Greenfoot.isKeyDown( "space" ) ){
+            getWorld().addObject( new gun(), getX(), getY() );
+            if( ! movingleft ){
+                movingleft = true;
+                move(5);
+            }
+            if( movingleft ){
+                movingleft = false;
+                 move(-5);
+            }
+        }*/
         if( Greenfoot.isKeyDown( "up" ) ){
             if( !up_pressed ){
                 up_pressed = true;
@@ -72,6 +83,7 @@ public class you extends Actor
         if( dy < 0 ){
             Actor block = getOneObjectAtOffset(-offset_x,-h/2+dy,brock.class);
             if( block == null ) block = getOneObjectAtOffset(offset_x,-h/2+dy,brock.class);
+            if( block == null ) block = getOneObjectAtOffset(0,-h/2+dy,brock.class);
             if( block != null ){
                 int by = block.getY();
                 int bh = block.getImage().getHeight();
